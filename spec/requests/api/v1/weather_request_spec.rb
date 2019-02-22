890667d43e27c99745e63c641948c4c7468b1397
Maddie Jones <maddyg91@gmail.com>
2019-02-21 18:12:14 -0700

    Setup for API done and First test written for forecast endpoint

---
+++ spec/requests/api/v1/weather_request_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'Weather API' do
+  it 'Can get weather by location' do
+    location = "denver,co"
+
+    get "/api/v1/forecast?location=#{location}"
+
+    forecast_json = JSON.parse(response.body)
+
+    expect(response).to be_successful
+  end
+end
