b6e1c3dbb37dadfe4069487cfe71d05e0ca904f7
Maddie Jones <maddyg91@gmail.com>
2019-02-25 10:08:35 -0700

    Adds api call to giphy

--- app/services/giphy_service.rb
+++ app/services/giphy_service.rb
@@ -1,5 +1,13 @@
 class GiphyService
   def self.weather_gifs(location)
     query = DarkSkyService.forecast(location)[:daily][:icon]
+    conn = Faraday.new(url: 'http://api.giphy.com') do |faraday|
+      faraday.adapter  Faraday.default_adapter
+      faraday.params[:api_key] = ENV["GIPHY_API_KEY"]
+      faraday.params[:q] = "snow"
+    end
+    response = conn.get '/v1/gifs/search'
+    binding.pry
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
