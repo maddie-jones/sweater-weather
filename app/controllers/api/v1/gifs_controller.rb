4134e6abd03406743d56389e49a1143eb6149fd6
Maddie Jones <maddyg91@gmail.com>
2019-02-25 11:10:27 -0700

    Test passing, json response has correct format.

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -9,8 +9,21 @@ class Api::V1::GifsController < ApplicationController
     summary = DarkSkyService.forecast(params[:location])[:daily][:data].map do |time|
       time[:summary]
     end
-    render status: :ok, json: {data: {images: [{ time: time,
-                                                 summary: summary,
-                                                 url: gif}]}}
+    render status: :ok, json: {data: {images: [{ time: time[0],
+                                                 summary: summary[0],
+                                                 url: gifs[0]},
+                                               { time: time[1],
+                                                 summary: summary[1],
+                                                 url: gifs[1]},
+                                               { time: time[2],
+                                                 summary: summary[2],
+                                                 url: gifs[2]},
+                                               { time: time[3],
+                                                 summary: summary[3],
+                                                 url: gifs[3]},
+                                               { time: time[4],
+                                                 summary: summary[4],
+                                                 url: gifs[4]}
+                                                   ]}}
   end
 end
