81317431319a120a7d2539674b01db029b5d9f14
Maddie Jones <maddyg91@gmail.com>
2019-02-25 15:09:39 -0700

    Adds authorization check for login. Adds error message :unothorized if password in incorrect. Adds test for both scenarios tests are passing

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    render status: :ok, json:  {api_key: user.api_key}
+    if user.try(:authenticate, params[:password])
+      render status: :ok, json:  {api_key: user.api_key}
+    else
+      render status: :unauthorized, json: {error: "401 unauthorized"}
+    end
   end
 end
--- spec/requests/api/v1/login_spec.rb
+++ spec/requests/api/v1/login_spec.rb
@@ -15,4 +15,18 @@ describe 'Login Api', :vcr do
 
     expect(JSON.parse(response.body)).to eq({"api_key" => user_1.api_key})
   end
+  it 'cannot login user with wrong password' do
+    user_1 = User.create(password: "password", email: "whatever@example.com", password_confirmation: "password")
+    user_2 = User.create(password: "password", email: "maddie@example.com", password_confirmation: "password")
+    params = {
+              email: "whatever@example.com",
+              password: "hello"
+    }
+
+    post "/api/v1/sessions", params: params
+
+    expect(response).to have_http_status(:unauthorized)
+
+    expect(JSON.parse(response.body)).to eq({"error" => "401 unauthorized"})
+  end
 end
