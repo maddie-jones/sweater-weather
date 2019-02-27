cabac38db71d0d18dc11734b408227b19c5b9c40
Maddie Jones <maddyg91@gmail.com>
2019-02-26 19:38:25 -0700

    Bug fix for user registrantion

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 describe 'Users API', :vcr do
   it 'can create user' do
-    params = {user: { email: "whatever@example.com",
+    params = { email: "whatever@example.com",
                password: "password",
-               password_confirmation: "password"}}
+               password_confirmation: "password"}
 
     post "/api/v1/users", params: params
 
     expect(response).to have_http_status(:created)
 
-    user = User.find_by(email: params[:user][:email])
+    user = User.find_by(email: params[:email])
 
     expect(JSON.parse(response.body)).to eq({"api_key" => user.api_key})
   end
